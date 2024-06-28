010d0f82109c8676e5ef71e12719c17d8f0efb02
austin <austinoblow97@gmail.com>
2024-06-28 15:49:24 -0400

    add link to go to individual shoe

--- client/src/components/ShoeCard.js
+++ client/src/components/ShoeCard.js
@@ -1,24 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const ShoeCard = ({ shoe }) => {
-    const { brand, image_url, model } = shoe
+    const { brand, image_url, model, id } = shoe
 
 
   return (
     <div className='shoe-card'>
+      <Link to={`/shoes/${id}`}>
         <h4>{brand} {model}</h4>
         <img 
         src={image_url}
         alt={brand}
         className="shoe-image" />
         <br/>
-          <button >Remove Shoe</button>
-          <div className="review-card">
-              <h4>Review by {shoe.user.username}</h4>
-              <p>{shoe.user.reviews[0].content}</p>
-          <div className="rating">Rating: {}/5</div>
-        </div>
+      </Link>
     </div>
   )
 }
