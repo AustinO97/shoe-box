1281baed82a934b97e6c81404a22882a95f83604
austin <austinoblow97@gmail.com>
2024-06-28 09:40:10 -0400

    add ValueError to post

--- server/resources/shoes.py
+++ server/resources/shoes.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, abort
 from flask_restful import Resource
 from config import db
 from models import Shoe
@@ -8,3 +8,32 @@ class Shoes(Resource):
         shoes = [shoe.to_dict() for shoe in Shoe.query.all()]
         return shoes, 200
     
+    def post(self):
+        data = request.get_json()
+
+        try:
+            new_shoe = Shoe(
+                model=data['model'],
+                brand=data['brand'],
+                image_url=data['image_url'],
+                user_id=data['user_id']
+            )
+            db.session.add(new_shoe)
+            db.session.commit()
+            return new_shoe.to_dict(), 201
+        
+        except ValueError as e:
+            abort(422, e.args[0])
+        
+
+class ShoesByID(Resource):
+
+    def get(self, id):
+        shoe = Shoe.query.filter(Shoe.id == id).first()
+        return shoe.to_dict(), 200
+
+    def delete(self, id):
+        shoe = Shoe.query.filter(Shoe.id == id).first()
+        db.session.delete(shoe)
+        db.session.commit()
+        return '', 204
