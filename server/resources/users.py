5fc72e2f40e408c7ab0a0f3c8a1e6b8e6933de4e
austin <austinoblow97@gmail.com>
2024-07-13 11:01:58 -0400

    add post

--- server/resources/users.py
+++ server/resources/users.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, abort
 from flask_restful import Resource
 from config import db
 from models import User
@@ -7,3 +7,18 @@ class Users(Resource):
     def get(self):
         users = [user.to_dict() for user in User.query.all()]
         return users, 200
+    
+    def post(self):
+        data = request.get_json()
+
+        try:
+            new_user = User(
+                username=data['username']
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+        except ValueError as e:
+            abort(422, e.args[0])
+
+        return new_user.to_dict(), 201
