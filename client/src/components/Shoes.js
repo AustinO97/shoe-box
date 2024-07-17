fd353d3b4635f4b6fb786b5ac1b6c9e136522b24
austin <austinoblow97@gmail.com>
2024-07-17 10:07:44 -0400

    move new shoe form out of shoes.js

--- client/src/components/Shoes.js
+++ client/src/components/Shoes.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
 import ShoeCard from './ShoeCard';
 
 const Shoes = () => {
@@ -14,38 +12,7 @@ const Shoes = () => {
       .catch(error => setError(error))
   }, [])
 
-  const validationSchema = yup.object({
-    brand: yup.string().required('Brand is required'),
-    model: yup.string().required('Model is required'),
-    image_url: yup.string().url('Invalid URL').required('Image URL is required')
-  })
 
-  const formik = useFormik({
-    initialValues: {
-      brand: '',
-      model: '',
-      image_url: ''
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values, { resetForm }) => {
-      fetch('/shoes', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(values),
-      })
-      .then(res => res.json())
-      .then(
-        (newShoe) => {
-          setShoes([...shoes, newShoe])
-          resetForm()
-        })
-        .catch((error) => {
-          setError(error)
-      })
-    }
-  })
 
   if (error) {
     return <div>Error: {error.message}</div>
@@ -58,52 +25,6 @@ const Shoes = () => {
   return (
     <div className='form-container'>
       <h1 className='header'>Shoe Box</h1>
-      <form id='shoe-form' onSubmit={formik.handleSubmit}>
-      <h3>Add New Shoe</h3>
-      <div>
-          <label htmlFor='brand'>Brand</label>
-          <input
-            id='brand'
-            name='brand'
-            type='text'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.brand}
-          />
-          {formik.touched.brand && formik.errors.brand ? (
-            <div>{formik.errors.brand}</div>
-          ) : null}
-        </div>
-        <div>
-          <label htmlFor='model'>Model</label>
-          <input
-            id='model'
-            name='model'
-            type='text'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.model}
-          />
-          {formik.touched.model && formik.errors.model ? (
-            <div>{formik.errors.model}</div>
-          ) : null}
-        </div>
-        <div>
-          <label htmlFor='image_url'>Image URL</label>
-          <input
-            id='image_url'
-            name='image_url'
-            type='text'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.image_url}
-          />
-          {formik.touched.image_url && formik.errors.image_url ? (
-            <div>{formik.errors.image_url}</div>
-          ) : null}
-        </div>
-        <button type='submit'>Add Shoe</button>
-      </form>
       <div className='shoe-container'>
         {startingShoes}
       </div>
