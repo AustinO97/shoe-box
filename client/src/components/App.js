d2be36aa6429ad807db277a60b0378322e8fce9e
austin <austinoblow97@gmail.com>
2024-07-17 10:12:21 -0400

    add add-shoe/about routes, add icons for links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,15 +4,21 @@ import Shoes from './Shoes';
 import Users from './Users';
 import Reviews from './Reviews';
 import ShoeDetail from './ShoeDetail';
+import ShoeForm from './ShoeForm';
+import About from './About';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUsers, faStar, faPlus, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div>
         <nav>
-          <NavLink exact to="/" activeClassName="active">Home</NavLink>
-          <NavLink to="/users" activeClassName="active">Users</NavLink>
-          <NavLink to="/reviews" activeClassName="active">Reviews</NavLink>
+          <NavLink exact to="/" activeClassName="active"><FontAwesomeIcon icon={faHome} />Home</NavLink>
+          <NavLink to="/users" activeClassName="active"><FontAwesomeIcon icon={faUsers} /> Users</NavLink>
+          <NavLink to="/reviews" activeClassName="active"><FontAwesomeIcon icon={faStar} /> Reviews</NavLink>
+          <NavLink to="/add-shoe" activeClassName="active"><FontAwesomeIcon icon={faPlus} /> New Shoe</NavLink>
+          <NavLink to="/about" activeClassName="active"><FontAwesomeIcon icon={faInfoCircle} /> About</NavLink>
         </nav>
 
         <Switch>
@@ -20,6 +26,8 @@ function App() {
           <Route path="/shoes/:id" component={ShoeDetail} />
           <Route path="/users" component={Users} />
           <Route path="/reviews" component={Reviews} />
+          <Route path="/add-shoe" component={ShoeForm} />
+          <Route path="/about" component={About} />
         </Switch>
       </div>
     </Router>
